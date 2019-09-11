af66a88d76bc84c9837dd932400bee40092bb720
Joaquin Tadeo <joaquintadeo@live.com.ar>
2019-09-11 19:49:46 -0300

    Created first test implementing framework

---
+++ miloqeev-framework/src/main/java/utils.java
@@ -0,0 +1,6 @@
+public class utils {
+    static public final int ID = 0;
+    static public final int NAME = 1;
+    static public final int CLASSNAME = 2;
+    static public final int XPATH = 3;
+}
---
+++ miloqeev-page-objects/src/main/java/pageObjects.java
@@ -0,0 +1,5 @@
+public class pageObjects {
+    static public final String googleUrl = "http://google.com";
+    static public final String searchBarr = "q";
+    static public final String facetXpath = "//div[@class='TbwUpd']//cite[text()='https://www.facet.unt.edu.ar']";
+}
