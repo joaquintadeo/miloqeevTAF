d7713398802828011df600089383159d789b671c
Joaquin Tadeo <joaquintadeo@live.com.ar>
2019-09-14 20:05:44 -0300

    clean

--- miloqeev-framework/src/main/java/UI/utils.java
+++
@@ -1,8 +0,0 @@
-package UI;
-
-public class utils {
-    static public final int ID = 0;
-    static public final int NAME = 1;
-    static public final int CLASSNAME = 2;
-    static public final int XPATH = 3;
-}
