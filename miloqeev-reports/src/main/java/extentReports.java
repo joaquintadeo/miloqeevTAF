6a5111603c61241be6926744966ec198d3433cd7
Joaquin Tadeo <joaquin.tadeo@sophos.com>
2019-09-25 13:21:38 -0300

    created method to generate reports

--- miloqeev-reports/src/main/java/extentReports.java
+++ miloqeev-reports/src/main/java/extentReports.java
@@ -1,30 +1,58 @@
+import UI.browserManagement;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class extentReports {
-    public void setupOnlyParent(){
+public class extentReports extends browserManagement {
+    ExtentHtmlReporter htmlReporter;
+    ExtentReports extent;
+    ExtentTest test;
 
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extent.html");
-        ExtentReports extent = new ExtentReports();
+    @BeforeMethod
+    public void setupReport(){
+        htmlReporter = new ExtentHtmlReporter("/Users/joaquin/Documents/framework/miloqeevTAF/miloqeev-reports/src/main/reports");
+        htmlReporter.loadXMLConfig("/Users/joaquin/Documents/framework/miloqeevTAF/miloqeev-reports/src/main/resources/extent-reports-config.xml");
+        extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
+    }
 
-        ExtentTest test = extent.createTest("Open Browser");
-        test.pass("Browser Opened");
-
+    @AfterMethod
+    public void flushReport(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
+            test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in extent report
+            String screenshotPath = extentReports.getScreenshot(driver, result.getName());
+            test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+        }
+        else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test Case PASSED IS " + result.getName());
+        }
         extent.flush();
     }
 
-    public void setupParentAndChild(){
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extent.html");
-        ExtentReports extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
-        ExtentTest parentTest = extent.createTest("Given_user_opens_a_browser");
+    public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
 
-        ExtentTest childTest1 = parentTest.createNode("Open chrome");
-        childTest1.pass("chrome opened");
-
-        parentTest.pass("Browser Opened");
-        extent.flush();
+        // after execution, you could see a folder "FailedTestsScreenshots" under src folder
+        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+        File finalDestination = new File(destination);
+        FileUtils.copyFile(source, finalDestination);
+        return destination;
     }
 }
