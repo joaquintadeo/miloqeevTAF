bf62aa6116ad60cb565ae4460cd7b6025cba19bd
Joaquin Tadeo <joaquin.tadeo@sophos.com>
2019-09-26 10:53:41 -0300

    adding method to create report

--- miloqeev-domain/src/main/java/facetPage/main.java
+++ miloqeev-domain/src/main/java/facetPage/main.java
@@ -2,27 +2,37 @@ package facetPage;
 import UI.functions;
 import UI.utils;
 import UI.browserManagement;
+import com.aventstack.extentreports.ExtentTest;
 
 public class main {
     private static int classname = utils.CLASSNAME;
     private static String facetLogo = facetLandingPage.facetLogo;
     private static String facetUrl = facetLandingPage.facetUrl;
+    ExtentTest test;
 
-    public static void Given_user_opens_a_browser(){
+    public main(ExtentTest test) {
+        this.test = test;
+    }
+
+    public void Given_user_opens_a_browser(){
+        test.createNode("Given_user_opens_a_browser");
         browserManagement.openBrowser("chrome");
         browserManagement.setBrowserImplicitWait(600);
         browserManagement.maximizeBrowserWindow();
     }
 
-    public static void When_user_navigates_to_facet_web_page(){
+    public void When_user_navigates_to_facet_web_page(){
+        test.createNode("When_user_navigates_to_facet_web_page");
         browserManagement.goTo(facetUrl);
     }
 
-    public static void Then_user_must_see_facet_logo(){
-        functions.PageShouldContainElement(classname, facetLogo);
+    public void Then_user_must_see_facet_logo(){
+        test.createNode("Then_user_must_see_facet_logo");
+        functions.PageShouldContainElement(classname, "trezo");
     }
 
-    public static void close_browser(){
+    public void close_browser(){
+        test.createNode("close_browser");
         browserManagement.closeBrowser();
     }
 }
--- miloqeev-tests/src/test/java/facetExtentReport.java
+++ miloqeev-tests/src/test/java/facetExtentReport.java
@@ -1,43 +1,79 @@
+import UI.browserManagement;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import facetPage.main;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class facetExtentReport {
+public class facetExtentReport extends browserManagement{
     ExtentHtmlReporter htmlReporter;
     ExtentReports extent;
+    ExtentTest test;
+    main steps;
 
     @BeforeMethod
     public void setup(){
-        htmlReporter = new ExtentHtmlReporter("C:\\Users\\Joaquin\\Documents\\Facultad\\Tesis\\miloqeevTAF\\miloqeev-reports\\src\\main\\reports\\extent.html");
-        htmlReporter.loadXMLConfig("C:\\Users\\Joaquin\\Documents\\Facultad\\Tesis\\miloqeevTAF\\miloqeev-reports\\src\\main\\resources\\extent-reports-config.xml");
-        extent = new ExtentReports();
-        extent.attachReporter(htmlReporter);
+        test = extent.createTest("MyFirstTest", "Sample description");
+        steps = new main(test);
     }
 
-    @AfterMethod
+    @AfterClass
     public void teardown(){
         extent.flush();
     }
 
     @Test
-    public void facetLogoExtentReport () throws Exception{
-        ExtentTest test = extent.createTest("MyFirstTest", "Sample description");
-        main.Given_user_opens_a_browser();
-        test.pass("User opens browser");
-        test.log(Status.INFO, "This step shows usage of log(status, details)");
-        test.fail("details", MediaEntityBuilder.createScreenCaptureFromPath("screenshot.png").build());
-        test.addScreenCaptureFromPath("screenshot.png");
-        main.When_user_navigates_to_facet_web_page();
-        test.pass("User goes to facet web page");
-        main.Then_user_must_see_facet_logo();
-        test.pass("User sees facet logo");
-        main.close_browser();
-        test.pass("User closes browser");
+    public void facetLogoExtentReport (){
+        steps.Given_user_opens_a_browser();
+        steps.When_user_navigates_to_facet_web_page();
+        steps.Then_user_must_see_facet_logo();
+        steps.close_browser();
+    }
+
+    @BeforeClass
+    public void createReport(){
+        htmlReporter = new ExtentHtmlReporter("/Users/joaquin/Documents/framework/miloqeevTAF/miloqeev-reports/src/main/reports/extent.html");
+        htmlReporter.loadXMLConfig("/Users/joaquin/Documents/framework/miloqeevTAF/miloqeev-reports/src/main/resources/extent-reports-config.xml");
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
+            test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in extent report
+            String screenshotPath = facetExtentReport.getScreenshot(driver, result.getName());
+            test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
+        }
+        else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(Status.PASS, "Test Case PASSED IS " + result.getName());
+        }
+        driver.quit();
+    }
+
+    public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+        String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+
+        // after execution, you could see a folder "FailedTestsScreenshots" under src folder
+        String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+        File finalDestination = new File(destination);
+        FileUtils.copyFile(source, finalDestination);
+        return destination;
     }
 }
