2b053200dd936d0b4ee287360a13460152974501
Joaquin Tadeo <joaquin.tadeo@sophos.com>
2020-04-08 10:40:23 -0300

    adding be methods

--- miloqeev-tests/src/test/java/tests/backend/getDummy.java
+++ miloqeev-tests/src/test/java/tests/backend/getDummy.java
@@ -1,16 +1,31 @@
 package tests.backend;
 
-import BE.*;
-import testListeners.extentReportListener;
 import cucumber.api.java.en.Given;
+import org.apache.http.Header;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
+import static BE.json.*;
+import static testListeners.extentReportListener.*;
+
 public class getDummy {
 
+//    @Given("User sends get request")
+//    public void userSendsGetRequest() throws IOException {
+//        createTest("Testing get request", "Get Dummy Endpoint", "User sends get request");
+//        getRequest("http://dummy.restapiexample.com/api/v1/employees");
+//        logJsonToConsole(responseJson);
+//        statusShouldBe(300);
+//    }
+
     @Given("User sends get request")
-    public void userSendsGetRequest() throws IOException {
-        extentReportListener.createTest("Testing get request", "Get Dummy Endpoint", "User sends get request");
-        json.getRequest("http://dummy.restapiexample.com/api/v1/employees");
+    public void userSendsGetRequest() throws IOException, ParseException {
+        createTest("Testing get request", "Get Dummy Endpoint", "User sends get request");
+        loadJsonFromFile("postDummy");
+        setRequestHeader("Content-Type", "application/json");
+        postRequest("http://dummy.restapiexample.com/api/v1/create");
+        logRespStatusToConsole();
+        saveJsonToFile("postDummyResponse2", responseJson);
     }
 }
